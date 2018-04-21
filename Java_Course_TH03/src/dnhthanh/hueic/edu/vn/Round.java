9470a06bc795055d2ab61bb5b1ca4d1a576c4a5d
NguyenHaiDuc <37949723+NguyenHaiDuc@users.noreply.github.com>
2018-04-21 16:25:02 +0700

    Add files via upload

--- Java_Course_TH03/src/dnhthanh/hueic/edu/vn/Round.java
+++ Java_Course_TH03/src/dnhthanh/hueic/edu/vn/Round.java
@@ -1,19 +1,34 @@
 package dnhthanh.hueic.edu.vn;
 
 public class Round {
-	private float R;//Bán kính
-	private Point O;//Tâm
-	
+	private float R;// Bán kính
+	private Point O;// Tâm
+
 	public Round() {
-		this.R = Math.round(Math.random()*100);
-		this.O = new Point();
+		this.setR(Math.round(Math.random() * 100));
+		this.setO(new Point());
 	}
-	
-	//Tạo getters và setters cho R và O
-	
-	//Xét vị trí tương đối điểm với đường tròn: nếu nằm trong - trả về -1; nếu nằm ngoài - trả về 1; nếu nằm trên - trả về 0
+
+	// Xét vị trí tương đối điểm với đường tròn: nếu nằm trong - trả về -1; nếu nằm
+	// ngoài - trả về 1; nếu nằm trên - trả về 0
 	public int ViTriTuongDoiDiemVoiDuongTron(Point A) {
-		
+
 		return 0;
+	}
+
+	public float getR() {
+		return R;
+	}
+
+	public void setR(float r) {
+		R = r;
+	}
+
+	public Point getO() {
+		return O;
+	}
+
+	public void setO(Point o) {
+		O = o;
 	}
 }
