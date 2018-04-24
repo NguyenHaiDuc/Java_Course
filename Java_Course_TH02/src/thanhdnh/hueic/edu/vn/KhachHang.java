3d1b7fe915baa22001d3a4325f27f4afe6d88983
NguyenHaiDuc <37949723+NguyenHaiDuc@users.noreply.github.com>
2018-04-24 08:15:06 +0700

    Update KhachHang.java

--- Java_Course_TH02/src/thanhdnh/hueic/edu/vn/KhachHang.java
+++ Java_Course_TH02/src/thanhdnh/hueic/edu/vn/KhachHang.java
@@ -1,35 +1,47 @@
 package thanhdnh.hueic.edu.vn;
 
+import java.util.Scanner;
+
 public class KhachHang {
 	private String username;
+	//Tiếp tục cho các thuộc tính khác
+
 	private String password;
+
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String un) {
-		username = un;
+
+	public void setUsername(String usn) {
+		username = usn;
 	}
+	//Tiếp tục cho các phương thức
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String pw) {
 		password = pw;
 	}
-	public KhachHang(String un, String pw) {
-		username = un;
+
+	public KhachHang(String usn, String pw) {
+		username = usn;
 		password = pw;
 	}
+
 	public KhachHang() {
 		username = "admin";
-		password = "admin123";
+		password = "admin";
 	}
-	public void MuaHang(String th , String nsx , double dg , int sl) {
-		HangHoa hh = new HangHoa();
-		hh.setTenHang(th);
-		hh.setNgaySanXuat(nsx);
-		hh.setDonGia(dg);
-		hh.setSoLuong(sl);
-		hh.TinhTien();
-		System.out.println(hh.HangHoa());
-	}
+
+	public String MuaHang(String th, String nsx, double dg, int sl) {
+			HangHoa hh = new HangHoa();
+			hh.setTenHang(th);
+			hh.setNgaySanXuat(nsx);
+			hh.setDonGia(dg);
+			hh.setSoLuong(sl);
+			hh.TinhTien();
+			return hh.toString();
+		}
 }
