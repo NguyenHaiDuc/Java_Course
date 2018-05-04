ac0c1fcdb61b93fb6d887d0982e5a5c0cad7fa4f
NguyenHaiDuc <37949723+NguyenHaiDuc@users.noreply.github.com>
2018-05-04 13:16:11 +0700

    Update Point.java

--- Java_Course_TH03/src/dnhthanh/hueic/edu/vn/Point.java
+++ Java_Course_TH03/src/dnhthanh/hueic/edu/vn/Point.java
@@ -3,26 +3,25 @@ package dnhthanh.hueic.edu.vn;
 public class Point {
 	private float x;// Tọa độ x
 	private float y;// Tọa độ y
-
+ 
 	public Point() {
 		this.setX(Math.round(Math.random() * 50));
 		this.setY(Math.round(Math.random() * 50));
 	}
 
-	public float getX() {
+	float getX() {
 		return x;
 	}
 
-	public void setX(float x) {
+	private void setX(float x) {
 		this.x = x;
 	}
 
-	public float getY() {
+	float getY() {
 		return y;
 	}
 
-	public void setY(float y) {
+	private void setY(float y) {
 		this.y = y;
 	}
-
 }
