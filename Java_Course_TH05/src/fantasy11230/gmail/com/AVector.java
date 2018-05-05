f95e5a87e4c5ebb22393a54576df312671d22172
NguyenHaiDuc <37949723+NguyenHaiDuc@users.noreply.github.com>
2018-05-05 16:27:58 +0700

    Add files via upload

---
+++ Java_Course_TH05/src/fantasy11230/gmail/com/AVector.java
@@ -0,0 +1,12 @@
+package fantasy11230.gmail.com;
+
+public abstract class AVector {
+	protected float x;
+	protected float y;
+
+	public abstract double tinhDoDai();
+
+	public abstract double sinGoc();
+
+	public abstract double trucGiao();
+}
---
+++ Java_Course_TH05/src/fantasy11230/gmail/com/Vector2D.java
@@ -0,0 +1,29 @@
+package fantasy11230.gmail.com;
+
+public class Vector2D extends AVector {
+	public Vector2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public double tinhDoDai() {
+		int a = 0;
+		// TODO Auto-generated method stub
+		return Math.abs(a);
+		
+	}
+
+	@Override
+	public double sinGoc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double trucGiao() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
